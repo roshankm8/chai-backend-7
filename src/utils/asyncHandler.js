12f1e4b59388dd26a18e7c3eaaf46a62b8db5fc5
unknown <roshankrm7@gmail.com>
2026-05-22 01:02:22 +0530

    DB Connection

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,16 @@
+const asyncHandler = (fn) => async (req,res,next)=>{
+    try {
+        await fn(req,res,next)
+        
+    } catch (error) {
+        res.status(error.code || 500).json({
+            success:false,
+            message:error.message
+        })
+        
+    }
+
+}
+
+
+// response or error k liye class constructor likha gya h
